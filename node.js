833dcc5d7bf1358e08bb4bcf12b51bb4e1aa1858
FatimaHarrison <106970872+FatimaHarrison@users.noreply.github.com>
2022-07-14 08:44:18 -0400

    Create node.js

---
+++ node.js
@@ -0,0 +1,17 @@
+var username = "FHarrison";
+var password = "*************"
+let output =('true + false')
+let username=propmt('Username')
+let password=propmt('Please enter your password')
+
+if(username === prompt){
+  output = true
+}
+else{
+  output = false
+  
+if(password === prompt){
+ console.log("Weclome back"+password)
+}
+else{
+ console.log("Try again"+password)
